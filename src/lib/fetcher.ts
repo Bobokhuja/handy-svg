6b1416b91e7e508a53fa60b960e3a25e279fe82b
Bobokhuja <65486207+Bobokhuja@users.noreply.github.com>
2023-04-14 20:50:58 +0500

    Update fetcher.ts

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,58 +1,51 @@
-import {checkSvgContent} from './utils';
+import { checkSvgContent } from './utils'
 
-const DEFAULT_TIMEOUT = 4800;
-const DEFAULT_RETRY_COUNT = 2;
+const DEFAULT_TIMEOUT = 4800
+const DEFAULT_RETRY_COUNT = 2
 
 interface RequestParams extends RequestInit {
-    timeout?: number;
-    retryCount?: number;
+  timeout?: number;
+  retryCount?: number;
 }
 
 async function fetchWithTimeout(url: string, params?: RequestParams) {
-    const timeout = params?.timeout || DEFAULT_TIMEOUT;
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), timeout);
+  const timeout = params?.timeout || DEFAULT_TIMEOUT
+  const controller = new AbortController()
+  const timeoutId = setTimeout(() => controller.abort(), timeout)
 
-    return fetch(url, {
-        ...params,
-        signal: controller.signal
-    })
-    .then((response) => {
-        clearTimeout(timeoutId);
-        return response;
-    })
-    .catch((error) => {
-        clearTimeout(timeoutId);
-        throw error;
-    })
+  try {
+    const response = await fetch(url, {...params, signal: controller.signal})
+    clearTimeout(timeoutId)
+    return response
+  } catch (error) {
+    throw error
+  } finally {
+    clearTimeout(timeoutId)
+  }
 }
 
+async function doFetch(url: string, params?: RequestParams): Promise<string> {
+  let retryCount = params?.retryCount === undefined
+    ? DEFAULT_RETRY_COUNT
+    : params?.retryCount
 
-export function fetchSvg(url: string, params?: RequestParams) {
-    let retryCount = params?.retryCount === undefined ? DEFAULT_RETRY_COUNT : params?.retryCount;
-
-    function doFetch(url: string, params?: RequestParams): Promise<string> {
-        return fetchWithTimeout(url, params)
-            .then((response) => {
-                if (response.ok) {
-                    return response.text();
-                }
-
-                throw new Error(`Unable to load SVG file: ${url}`);
-            })
-            .catch((error) => {
-                if (retryCount > 0) {
-                    retryCount--;
-                    return doFetch(url, params);
-                }
-
-                throw error;
-            })
+  try {
+    const response = await fetchWithTimeout(url, params)
+    if (response.ok) {
+      return response.text()
     }
+    throw new Error(`Unable to load SVG file: ${url}`)
+  } catch (error) {
+    if (retryCount > 0) {
+      retryCount--
+      return doFetch(url, params)
+    }
+    throw error
+  }
+}
 
-    return doFetch(url, params)
-        .then((response) => {
-            checkSvgContent(response);
-            return response;
-        });
+export async function fetchSvg(url: string, params?: RequestParams) {
+  const response = await doFetch(url, params)
+  checkSvgContent(response)
+  return response
 }
